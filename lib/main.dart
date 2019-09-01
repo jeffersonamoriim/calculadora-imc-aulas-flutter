215aead5b0f28ee66ec72de3cfdea0352c0ee945
Jefferson Amorim <37017888+jeffersonamoriim@users.noreply.github.com>
2019-09-01 18:47:38 -0300

    class person.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:calculadora_imc/models/person.dart';
 
 void main() => runApp(
       MaterialApp(
---
+++ lib/models/person.dart
@@ -0,0 +1,6 @@
+class Person{
+  double weight, height;
+  int gender;
+
+  Person({this.weight, this.height, this.gender});
+}
