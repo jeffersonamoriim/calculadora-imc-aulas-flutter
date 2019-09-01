0bd6eed30d38736da15512d3e1abd777b4d5c0c7
Jefferson Amorim <37017888+jeffersonamoriim@users.noreply.github.com>
2019-09-01 17:10:59 -0300

    #DesafioParaAula
    Completed Tasks on README.md

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,13 @@ void main() => runApp(
       ),
     );
 
+class Person{
+  double weight, height;
+  int gender;
+
+  Person({this.weight, this.height, this.gender});
+}
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -18,11 +25,20 @@ class _HomeState extends State<Home> {
   TextEditingController _weightController = TextEditingController();
   TextEditingController _heightController = TextEditingController();
   String _result;
+  double _imc = 0;
+  int _selectedRadio;
 
   @override
   void initState() {
     super.initState();
     resetFields();
+    _selectedRadio = 0;
+  }
+
+  void _setSelectRadio(int value){
+    setState(() {
+     _selectedRadio = value; 
+    });
   }
 
   void resetFields() {
@@ -57,8 +73,42 @@ class _HomeState extends State<Home> {
     );
   }
 
+
+  Widget _buildRadioButtom(){
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Text("Masculino",
+        style: new TextStyle(
+          fontSize: 15.0,
+        )),
+        new Radio(
+          value: 1,
+          groupValue: _selectedRadio,
+          onChanged: (value) {
+            print("Radio $value");
+            _setSelectRadio(value);
+          },
+        ),
+        new Text("Feminino",
+        style: new TextStyle(
+          fontSize: 15.0,
+        )),
+        new Radio(
+          value: 2,
+          groupValue: _selectedRadio,
+          onChanged: (value) {
+            print("Radio $value");
+            _setSelectRadio(value);
+          },
+        ),
+      ],
+    );
+  }
+
   Form buildForm() {
-    return Form(
+    return  
+    Form(
       key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -71,6 +121,7 @@ class _HomeState extends State<Home> {
               label: "Altura (cm)",
               error: "Insira uma altura!",
               controller: _heightController),
+          _buildRadioButtom(),
           buildTextResult(),
           buildCalculateButton(),
         ],
@@ -81,47 +132,80 @@ class _HomeState extends State<Home> {
   void calculateImc() {
     double weight = double.parse(_weightController.text);
     double height = double.parse(_heightController.text) / 100.0;
-    double imc = weight / (height * height);
+    _imc = weight / (height * height);
 
-    setState(() {
-      _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
-      if (imc < 18.6)
-        _result += "Abaixo do peso";
-      else if (imc < 25.0)
+    if (_selectedRadio == 1) {
+      setState(() {
+      _result = "IMC = ${_imc.toStringAsPrecision(3)}\n";
+      if (_imc < 20.7)
+        _result += "Abaixo do Peso";
+      else if (_imc < 26.4)
         _result += "Peso ideal";
-      else if (imc < 30.0)
+      else if (_imc < 27.8)
         _result += "Levemente acima do peso";
-      else if (imc < 35.0)
-        _result += "Obesidade Grau I";
-      else if (imc < 40.0)
-        _result += "Obesidade Grau II";
+      else if (_imc < 31.1)
+        _result += "Acima do Peso";
       else
-        _result += "Obesidade Grau IIII";
+        _result += "Obesidade";
     });
+    }
+    else if (_selectedRadio == 2){
+      setState(() {
+      _result = "IMC = ${_imc.toStringAsPrecision(3)}\n";
+      if (_imc < 19.1)
+      _result += "Abaixo do peso";
+      else if (_imc < 25.8)
+        _result += "Peso ideal";
+      else if (_imc < 27.3)
+        _result += "Levemente acima do peso";
+      else if (_imc < 32.3)
+        _result += "Acima do Peso";
+      else
+        _result += "Obesidade";
+    });
+    }
   }
 
   Widget buildCalculateButton() {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 36.0),
+      padding: EdgeInsets.symmetric(vertical: 15.0),
       child: RaisedButton(
         onPressed: () {
           if (_formKey.currentState.validate()) {
             calculateImc();
           }
         },
+        color: Colors.blueAccent,
         child: Text('CALCULAR', style: TextStyle(color: Colors.white)),
       ),
     );
   }
 
   Widget buildTextResult() {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 36.0),
-      child: Text(
-        _result,
-        textAlign: TextAlign.center,
-      ),
-    );
+    if (_selectedRadio == 1){
+    if (_imc < 20.7)
+    return buildPaddingColors(Colors.orange);
+    else if (_imc < 26.4)
+    return buildPaddingColors(Colors.green);
+    else if (_imc < 27.8)
+    return buildPaddingColors(Colors.yellow[900]);
+    else if (_imc < 31.1)
+    return buildPaddingColors(Colors.orange[900]);
+    else
+    return buildPaddingColors(Colors.redAccent[700]);
+    }
+    else if (_selectedRadio == 2){
+    if (_imc < 19.1)
+    return buildPaddingColors(Colors.orange);
+    else if (_imc < 25.8)
+    return buildPaddingColors(Colors.green);
+    else if (_imc < 27.3)
+    return buildPaddingColors(Colors.yellow[900]);
+    else if (_imc < 32.3)
+    return buildPaddingColors(Colors.orange[900]);
+    else
+    return buildPaddingColors(Colors.redAccent[700]);
+    }
   }
 
   Widget buildTextFormField(
@@ -134,5 +218,19 @@ class _HomeState extends State<Home> {
         return text.isEmpty ? error : null;
       },
     );
+  }
+
+  Widget buildPaddingColors(Color color) {
+    return Padding(
+    padding: EdgeInsets.symmetric(vertical: 36.0),
+    child: Text(
+      _result,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: color,
+        fontSize: 22.0,
+        fontWeight: FontWeight.bold, ),
+    ),
+  );
   }
 }
